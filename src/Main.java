9a7605e2073dc5f681c347f962664aa770af220a
Daiquiri <mestelo@inbox.ru>
2024-12-29 01:25:00 +0100

    2.1 step

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
-public class Main extends JFrame {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame implements ActionListener {
     public static void main(String[] args) {
         Timer f = new Timer();
         //здесь надо вызвать метод, который отвечает за фиксацию времени
@@ -8,16 +11,46 @@ public class Main extends JFrame {
         //метод, вычитающий время потраченное на обучение, вывод на экран
         //сохранить результаты в файл
         //дополнительные функции? например - это тратил на чтение, это на просмотр ютуба, если это имеет смысл
+        //класс для хранения записей о времени
+        //класс для управления временем
+        //взаимодействие с пользователем
+        //это только черновик с наработками, поэтому бардак. уберусь
 
         f.timeFixer();
         Main m = new Main();
+
 
     }
     public Main(){
         setTitle("Fix Learning");//это название программы будет видно вверху окна Windows
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//Это крестик, чтоб закрыть окно
-        setSize(320,345);//Это будет маленькое окошко 320 на 345 пикселей, не квадрат, потому что говорят учитывать верхнюю рамку
+        JButton start = new JButton("Start");
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //тут метод, запускающий таймер
+                //пока просто заглушка
+                JOptionPane.showMessageDialog(null,"Start learning");
+            }
+        });
+        add(start);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        mainPanel.add(start);
+        JButton stop = new JButton("Stop");
+        setSize(500,320);
         setLocation(400,400);//Это просто чтоб окошко вызывалось не впритык слева вверху экрана
         setVisible(true);//без этого метода не увидеть вызванного окна
+
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command.equals("Start")){
+
+        }
     }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-class UserInterface extends JPanel {
+class UserInterface extends JPanel implements ActionListener {
     private final int SIZE_WINDOW = 320;
     private Image fon;
 
@@ -12,5 +14,11 @@ class UserInterface extends JPanel {
 
     void loadImage(){
         ImageIcon backGroung = new ImageIcon();
+        backGroung.getImage();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
